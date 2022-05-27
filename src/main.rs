4054990f54756fbd2ee5ea1f9e01745e971b3c60
Luis de la Cruz <daniking@pop-os.localdomain>
2022-05-27 07:43:53 -0400

    Better menu tui and more logic work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crossterm::{
     event::{self, Event, KeyCode},
-    terminal::{enable_raw_mode, disable_raw_mode},
+    terminal::{enable_raw_mode, disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}, execute,
 };
 use util::StatefulList;
 use std::{io, time::{Duration, Instant}};
@@ -14,18 +14,17 @@ mod util;
 mod test;
 fn main() -> Result<(), io::Error> {
     enable_raw_mode()?;
-    let  stdout = io::stdout();
-
-    let backend = CrosstermBackend::new(stdout);
-
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);    
     let mut terminal = Terminal::new(backend)?;
     let mut app = App::new();
     run(&mut app, &mut terminal)?;
     disable_raw_mode()?;
-
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
     Ok(())
-    
 }
+
 fn run<B: Backend>(app: &mut App, terminal: &mut Terminal<B>) -> Result<(), io::Error> {
     let tick_rate = Duration::from_millis(250);
     let mut last_tick = Instant::now();
@@ -47,8 +46,21 @@ fn handle_events(app: &mut App,timeout: Duration) -> Result<(), io::Error> {
         // If this is a keyboard event
         if let Event::Key(key) = event::read()? {
             match key.code {
-                KeyCode::Down => app.data.next(),
-                KeyCode::Up => app.data.previous(),
+                KeyCode::Down => app.state.next(),
+                KeyCode::Up => app.state.previous(),
+                KeyCode::Enter => {
+                    let i = app.state.selected();
+                    match i {
+                        Some(val) => {
+                            match val {
+                                0 => println!("Generating item json...\r"),
+                                1 => println!("Generating block json...\r"),
+                                _ => (),
+                            }
+                        },
+                        None => (),
+                    }
+                }
                 _ => {}
             }
         }
@@ -61,41 +73,52 @@ fn render_app<B: Backend>(app: &mut App, frame: &mut Frame<B>) {
     .constraints([Constraint::Percentage(100)].as_ref())
     .split(frame.size());
 
-    let items: Vec<ListItem> = app.data
+    let items: Vec<ListItem> = app.state
     .list
         .iter()
         .map(|entry| {
-            let mut lines = vec![Spans::from(entry.as_ref())];
+            let mut lines = vec![
+                Spans::from(entry.option)
+            ];
             lines.push(Spans::from(Span::styled(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                entry.desc,
                 Style::default().add_modifier(Modifier::ITALIC))));
                 return ListItem::new(lines).style(Style::default().fg(Color::White));
             }).collect();
     let list = List::new(items)
-    .block(Block::default().borders(Borders::ALL).title("List"))  .highlight_style(
+    .block(Block::default().borders(Borders::ALL).title("Options"))  .highlight_style(
         Style::default()
-            .bg(Color::LightGreen)
-            .add_modifier(Modifier::BOLD),
-    ).highlight_symbol(">> ");
-    frame.render_stateful_widget(list, area[0], &mut app.data.state);
+            .fg(Color::Rgb(255, 153, 0))
+            .add_modifier(Modifier::BOLD)
+    ).highlight_symbol(" > ");
+    frame.render_stateful_widget(list, area[0], &mut app.state.current);
 }
 
 /// This struct holds the current state of the app.
 struct App<'a> {
-    data: StatefulList<&'a str>,
+    state: StatefulList<AvailableOption<'a>>,
 }
+
 impl<'a> App<'a> {
     pub fn new() -> Self {
         
         Self {
-            data: StatefulList::of(vec![
-                "Item1",
-                "Item2",
-                "Item3",
+            state: StatefulList::of(vec![
+                AvailableOption::new("Create Item\n\n", "Generates JSON files for an item."),
+                AvailableOption::new("Create Block", "Generates JSON files for a block."),
             ]),
         }
     }
     pub fn tick(&self) {
 
     }
+}
+struct AvailableOption<'a> {
+    option: &'a str,
+    desc: &'a str
+}
+impl <'a> AvailableOption<'a> {
+    pub fn new(option: &'a str, desc: &'a str) -> AvailableOption<'a>{
+        Self { option: option, desc: desc }
+    } 
 }
--- src/util.rs
+++ src/util.rs
@@ -1,19 +1,23 @@
 use tui::widgets::ListState;
 
 pub struct StatefulList<T> {
-    pub state: ListState,
+    pub current: ListState,
     pub list: Vec<T>,
 }
 
 impl <T> StatefulList<T> {
     pub fn of(list: Vec<T>) -> Self {
         Self { 
-            state: ListState::default(),
+            current: ListState::default(),
             list: list,
         }
     }
+
+    pub fn selected(&self) -> Option<usize> {
+        self.current.selected()
+    }
     pub fn next(&mut self) {
-        let i = match self.state.selected() {
+        let i = match self.current.selected() {
             Some(i) => {
                 if i >= self.list.len() - 1 {
                     0
@@ -23,10 +27,10 @@ impl <T> StatefulList<T> {
             }
             None => 0,
         };
-        self.state.select(Some(i));
+        self.current.select(Some(i));
     }
     pub fn previous(&mut self) {
-        let i = match self.state.selected() {
+        let i = match self.current.selected() {
             Some(i) => {
                 if i == 0 {
                     self.list.len() - 1
@@ -36,6 +40,6 @@ impl <T> StatefulList<T> {
             }
             None => 0,
         };
-        self.state.select(Some(i));
+        self.current.select(Some(i));
     }
 } 
